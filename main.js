3a8f39ba5c554ef9a8e1ca2448b5183d197a8509
Wendy2567 <88495576+Wendy2567@users.noreply.github.com>
2021-08-14 21:38:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+// personal greeting
+let mybutton = document.querySelector('button');
+let myheader = document.getElementById('hw');
+
+function setUserName() {
+  let myname = prompt('Please enter your name.');
+  if(!myname) {
+    setUserName();
+  } else {
+  localStorage.setItem('name', myname);
+  myheader.textContent = 'Hello World, ' + myname;
+} }
+
+if(!localStorage.getItem('name')) {setUserName();}
+else {
+    let storedname = localStorage.getItem('name');
+    myheader.textContent = 'Hello World, ' + storedname;
+}
+
+mybutton.onclick = function() {setUserName();}
+
+let saybye = document.getElementById('Bye')
+saybye.onclick = function()  {saybye.textContent = 'LoVe you~'}
